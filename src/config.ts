a7c82d24b6dd4283c4d73ca69203096c0962b850
Fred <83474597+wilfredobeng@users.noreply.github.com>
2023-05-09 23:27:25 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const LOCALE = ["en-EN"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: true,
-  svg: true,
+  svg: false,
   width: 216,
   height: 46,
 };
