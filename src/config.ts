870feb2162b6dee9ffe148ba5925e9b9da580de5
Fred <wilfred.obeng@stu.ucc.edu.gh>
2023-05-10 01:28:02 +0000

    CHANGED title

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   website: "http://wilfredd.me/",
   author: "Wilfred Obeng",
   desc: "The blog of a medical student who is interested in programming",
-  title: "ME.write()",
+  title: "WILFRED.OBENG ",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
